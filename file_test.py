a4c1bf42bb7fe52399051b0a16951776e0291681
Gonzalo <ubuntu@ip-172-31-36-247.eu-west-1.compute.internal>
2023-10-23 12:27:11 +0000

    fichier control_tests corrigé 2

--- file_test.py
+++ file_test.py
@@ -1,6 +1,3 @@
-import pytest
-
-
 def test_calc_addition():
     # Fonction test du résultat de 2+4
     output = 2+4
@@ -20,6 +17,6 @@ def test_calc_multiply():
 
 
 def test_coucou():
-     # Fonction test si la résultat renvoie 'hello'
+    # Fonction test si la résultat renvoie 'hello'
     output = 'hello'
     assert output == 'hello'
